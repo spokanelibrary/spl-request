10ad6dec13c757f297565cb969508364cd2fc2b3
Sean Girard <seangirard@yahoo.com>
2016-08-16 09:02:52 -0700

    rt

--- spl-request.php
+++ spl-request.php
@@ -60,7 +60,7 @@ class SPL_Request {
 			}	
 			$i = 1;
 			foreach ( $tickets as $id => $ticket ) {
-				$dash .= '<div class="col-col-sm-6">'.PHP_EOL;
+				$dash .= '<div class="col-sm-6">'.PHP_EOL;
 				$dash .= '<div class="panel panel-default">'.PHP_EOL;
 				$dash .= '<div class="panel panel-body">'.PHP_EOL;
 				$dash .= '<h4>'.$ticket['subject'].'</h4>'.PHP_EOL;

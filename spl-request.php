eebd3cc6119c4acec512d35413f5fa126dff4d0b
Sean Girard <seangirard@yahoo.com>
2016-08-16 11:03:15 -0700

    rt

--- spl-request.php
+++ spl-request.php
@@ -37,7 +37,7 @@ class SPL_Request {
 	}
 
 	public function getDashboard() {
-		$open = $this->rt->search("Queue='Automation'ANDStatus='new'",'-Created', 's');
+		$open = $this->rt->search("Queue='Automation'(ANDStatus='new'ORStatus='open')",'-Created', 's');
 		unset($open['']);
 
 		$tickets = array();

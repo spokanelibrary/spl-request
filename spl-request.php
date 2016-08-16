b055311ba691add94c1f202ac4eb15b1b90ed2ff
Sean Girard <seangirard@yahoo.com>
2016-08-16 11:49:15 -0700

    rt

--- spl-request.php
+++ spl-request.php
@@ -115,7 +115,7 @@ class SPL_Request {
 
 
 				$dash .= '<div class="modal fade" id="spl-rt-'.$id.'" tabindex="-1" role="dialog">'.PHP_EOL;
-				//$dash .= '<div class="modal-dialog" role="document">'.PHP_EOL;
+				$dash .= '<div class="modal-dialog" role="document">'.PHP_EOL;
 				//$dash .= '<div class="modal-content">'.PHP_EOL
 				//$dash .= '<div class="modal-header">'.PHP_EOL;
 				//$dash .= '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.PHP_EOL;
@@ -132,7 +132,7 @@ class SPL_Request {
 				//$dash .= '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'.PHP_EOL;
 				//$dash .= '</div>'.PHP_EOL;
 				//$dash .= '</div>'.PHP_EOL;
-				//$dash .= '</div>'.PHP_EOL;
+				$dash .= '</div>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
 
 				

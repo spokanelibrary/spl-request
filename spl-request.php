201cf06913009d9e242beef1ac7917951284b89d
Sean Girard <seangirard@yahoo.com>
2016-08-16 12:50:36 -0700

    rt

--- spl-request.php
+++ spl-request.php
@@ -99,8 +99,7 @@ class SPL_Request {
 				$dash .= '<div class="row">'.PHP_EOL;
 				$dash .= '<div class="col-sm-12">'.PHP_EOL;
 				$dash .= '<h5>';
-				//$dash .= '<a href="#" data-toggle="modal" data-target="#spl-rt-'.$id.'"><i class="glyphicon glyphicon-share'.$ticket['subject'].'</a>';
-				$dash .= '<a href="#" data-toggle="modal" data-target="#spl-rt-'.$id.'"><i class="glyphicon glyphicon-share"></i>'.$ticket['subject'].'</a>';
+				$dash .= '<a href="#" data-toggle="modal" data-target="#spl-rt-'.$id.'"><i class="glyphicon glyphicon-share"></i> '.$ticket['subject'].'</a>';
 				$dash .= '</h5>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
@@ -115,7 +114,6 @@ class SPL_Request {
 				$dash .= '</div>'.PHP_EOL;
 
 
-				/*
 				$dash .= '<div class="modal hide" id="spl-rt-'.$id.'" tabindex="-1" role="dialog">'.PHP_EOL;
 				$dash .= '<div class="modal-dialog" role="document">'.PHP_EOL;
 				$dash .= '<div class="modal-content">'.PHP_EOL;
@@ -138,7 +136,6 @@ class SPL_Request {
 				$dash .= '</div>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
-				*/
 				
 
 				$dash .= '</div>'.PHP_EOL;

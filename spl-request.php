5ca4bad17e4d0602ca57b8e63d1abb65145fd4fd
Sean Girard <seangirard@yahoo.com>
2016-08-16 14:03:04 -0700

    rt

--- spl-request.php
+++ spl-request.php
@@ -91,10 +91,10 @@ class SPL_Request {
 				$dash .= '<div class="panel-body">'.PHP_EOL;
 				
 				$dash .= '<div class="row">'.PHP_EOL;
-				$dash .= '<div class="col-sm-6">'.PHP_EOL;
-				$dash .= '<h5 class="help-block">'.$id.'</h5>'.PHP_EOL;
+				$dash .= '<div class="col-sm-5">'.PHP_EOL;
+				$dash .= '<h5 class="">'.$id.'</h5>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
-				$dash .= '<div class="col-sm-3">'.PHP_EOL;
+				$dash .= '<div class="col-sm-4">'.PHP_EOL;
 				$dash .= '<h5 class=""><small class="help-block">'.$ticket['properties']['Created'].'</small></h5>'.PHP_EOL;
 				$dash .= '</div>'.PHP_EOL;
 				$dash .= '<div class="col-sm-2">'.PHP_EOL;
